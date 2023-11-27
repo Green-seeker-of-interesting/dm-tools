b19f1a3f019408f177e7f011c57fcadb8cfa9442
Блинов_Егор_Сергеевич <ES_Blinov@BNIPI.rosneft.ru>
2023-11-27 23:26:00 +0500

    add config gui

--- setting.py
+++ setting.py
@@ -1,1 +1,11 @@
+# Database Settings
 DB_NAMES = "main_db.db"
+
+# Setting up the main window
+MAIN_WINDOW_WIDTH = 1600
+MAIN_WINDOW_HEIGHT = 1000
+
+RESIZABLE = False
+
+# Setting main menu
+MAIN_MENU_WIDTH = 200
